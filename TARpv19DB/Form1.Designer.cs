2322b2053e44f2ace554078b85039b4274f25599
Veronika <veronika.jefimova03@gmai.com>
2020-12-11 16:00:22 +0200

    Добавление интерфейса

--- TARpv19DB/Form1.Designer.cs
+++ TARpv19DB/Form1.Designer.cs
@@ -28,13 +28,115 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loginTXT = new System.Windows.Forms.TextBox();
+            this.passTXT = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.avtorizatsiaBTN = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(430, 95);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(117, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Авторизация";
+            // 
+            // loginTXT
+            // 
+            this.loginTXT.Location = new System.Drawing.Point(123, 168);
+            this.loginTXT.Name = "loginTXT";
+            this.loginTXT.Size = new System.Drawing.Size(247, 22);
+            this.loginTXT.TabIndex = 1;
+            // 
+            // passTXT
+            // 
+            this.passTXT.Location = new System.Drawing.Point(123, 241);
+            this.passTXT.Name = "passTXT";
+            this.passTXT.PasswordChar = '*';
+            this.passTXT.Size = new System.Drawing.Size(247, 22);
+            this.passTXT.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(39, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Логин";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(39, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Пароль";
+            // 
+            // avtorizatsiaBTN
+            // 
+            this.avtorizatsiaBTN.BackColor = System.Drawing.Color.Black;
+            this.avtorizatsiaBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.avtorizatsiaBTN.ForeColor = System.Drawing.Color.White;
+            this.avtorizatsiaBTN.Location = new System.Drawing.Point(140, 346);
+            this.avtorizatsiaBTN.Name = "avtorizatsiaBTN";
+            this.avtorizatsiaBTN.Size = new System.Drawing.Size(131, 49);
+            this.avtorizatsiaBTN.TabIndex = 5;
+            this.avtorizatsiaBTN.Text = "Готово";
+            this.avtorizatsiaBTN.UseVisualStyleBackColor = false;
+            this.avtorizatsiaBTN.Click += new System.EventHandler(this.avtorizatsiaBTN_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(430, 431);
+            this.Controls.Add(this.avtorizatsiaBTN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.passTXT);
+            this.Controls.Add(this.loginTXT);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Авторизация";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox loginTXT;
+        private System.Windows.Forms.TextBox passTXT;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button avtorizatsiaBTN;
     }
 }
 
--- TARpv19DB/Form1.cs
+++ TARpv19DB/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,24 @@ namespace TARpv19DB
 {
     public partial class Form1 : Form
     {
+        SqlConnection connect = new SqlConnection(@"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename =|DataDirectory|\AppData\opilased.mdf; Integrated Security = True");
+        SqlCommand command;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void avtorizatsiaBTN_Click(object sender, EventArgs e)
+        {
+            if(loginTXT.Text != "" && passTXT.Text != "")
+            {
+                connect.Open();
+                command = new SqlCommand("SELECT * FROM avtorizatsia WHERE login=@log,password=@pass", connect);
+                command.Parameters.AddWithValue("@log",loginTXT);
+                command.Parameters.AddWithValue("@pass", passTXT);
+
+                connect.Close();
+            }
         }
     }
 }
